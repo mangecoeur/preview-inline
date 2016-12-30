b57865cbd169fda192f66c2a5d071d7e764081d7
Jonathan Chambers <jon.chambers3001@gmail.com>
2016-12-30 18:55:17 +0100

    Added support for previewing citations if a biblography provider is present

--- spec/image-marker-spec.js
+++ spec/image-marker-spec.js
@@ -25,7 +25,7 @@ describe("ImageMarker", () => {
       atom.workspace.open(filePath).then(function(ed) {
         editor = ed;
         let grammar = atom.grammars.grammarForScopeName('source.gfm');
-        return editor.setGrammar(grammar);
+        editor.setGrammar(grammar);
       })
     );
 
@@ -40,35 +40,35 @@ describe("ImageMarker", () => {
       expect(ImageMarker.parseImageLocation(url)).toEqual(url);
 
       // should work when the file basePath is provided
-      return expect(ImageMarker.parseImageLocation(url, __dirname)).toEqual(url);
+      expect(ImageMarker.parseImageLocation(url, __dirname)).toEqual(url);
     });
 
     it("parses an absolute path", () => {
       let imgPath = path.join(__dirname, "test-image.jpg");
-      return expect(ImageMarker.parseImageLocation(imgPath)).toEqual(imgPath);
+      expect(ImageMarker.parseImageLocation(imgPath)).toEqual(imgPath);
     });
 
     it("parses a relative path, given a basePath", () => {
       let imgPath = "test-image.jpg";
-      return expect(ImageMarker.parseImageLocation(imgPath, __dirname))
+      expect(ImageMarker.parseImageLocation(imgPath, __dirname))
         .toEqual(path.join(__dirname, "test-image.jpg"));
     });
 
     it("throws an error for absolute path of a file that doesn't exist", () => {
       let imgPath = path.join(__dirname, "non-image.jpg");
-      return expect(() => ImageMarker.parseImageLocation(imgPath))
+      expect(() => ImageMarker.parseImageLocation(imgPath))
         .toThrow(new Error(`no image ${imgPath}`));
     });
 
     it("throws an error for a relative path of a file that doesn't exist", () => {
       let imgPath = "non-image.jpg";
-      return expect(() => ImageMarker.parseImageLocation(imgPath, __dirname))
+      expect(() => ImageMarker.parseImageLocation(imgPath, __dirname))
         .toThrow(new Error(`no image ${imgPath}`));
     });
 
-    return it("throws an error for a relative path if there is no basePath", () => {
+    it("throws an error for a relative path if there is no basePath", () => {
       let imgPath = "test-image.jpg";
-      return expect(() => ImageMarker.parseImageLocation(imgPath))
+      expect(() => ImageMarker.parseImageLocation(imgPath))
         .toThrow(new Error(`no image ${imgPath}`));
     });
   });
